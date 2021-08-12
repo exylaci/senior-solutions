a0f816ec0f6d1714a85de23d4713ed769539d198
exylaci <exy@freemail.hu>
2021-08-13 00:42:15 +0200

    Záró vizsga - leforduló váz  • elvárt tesztesetek kikommentezve  • kódoláshoz előkészítés  • pom.xml kibővítve: flyway, modelmapper, zalando  • layered dockerfile kész

--- exams/sv2021-jvjbf-zarovizsga/src/main/java/org/training360/finalexam/ZarovizsgaApplication.java
+++ exams/sv2021-jvjbf-zarovizsga/src/main/java/org/training360/finalexam/ZarovizsgaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Sv2021JvjbfZarovizsgaApplication {
+public class ZarovizsgaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Sv2021JvjbfZarovizsgaApplication.class, args);
+        SpringApplication.run(ZarovizsgaApplication.class, args);
     }
 
 }
---
+++ exams/sv2021-jvjbf-zarovizsga/src/main/resources/db/migration/V1__empty.sql
@@ -0,0 +1,1 @@
+create table players (id bigint not null auto_increment primary key);
