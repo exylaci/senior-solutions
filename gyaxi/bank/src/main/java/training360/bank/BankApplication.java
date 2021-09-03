c09eebacbc0bc23584ce44357d5015f4a10d6f5b
exylaci <exy@freemail.hu>
2021-09-03 18:02:43 +0200

    Bank applikáció - leforduló váz
    -dockerfile
    -adatbázis elérés

--- gyaxi/bank/src/main/java/training360/bank/BankApplication.java
+++ gyaxi/bank/src/main/java/training360/bank/BankApplication.java
@@ -1,7 +1,9 @@
 package training360.bank;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BankApplication {
@@ -10,4 +12,8 @@ public class BankApplication {
         SpringApplication.run(BankApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
 }
