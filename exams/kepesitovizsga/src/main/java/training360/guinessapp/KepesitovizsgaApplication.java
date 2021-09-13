158fdf3508e5a7cae92dc5eccca8fe72aaae7b88
exylaci <exy@freemail.hu>
2021-09-13 18:51:29 +0200

    képesítő vizsga - leforduló váz  • pom.xml kiegészítése  • docker file létrehozása  • feladat megfogalmazása  • adatbázis elérés beállítása

--- exams/kepesitovizsga/src/main/java/training360/guinessapp/KepesitovizsgaApplication.java
+++ exams/kepesitovizsga/src/main/java/training360/guinessapp/KepesitovizsgaApplication.java
@@ -1,11 +1,18 @@
 package training360.guinessapp;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KepesitovizsgaApplication {
     public static void main(String[] args) {
         SpringApplication.run(KepesitovizsgaApplication.class, args);
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
     }
 }
